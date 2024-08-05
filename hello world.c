66969d05ee2e2f70f9fee79d12991d6beb72fa2f
Try20240803 <370756208@qq.com>
2024-08-05 10:21:32 +0800

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+	int num; 
+	num = 99;
+	
+	printf("HELLO WORLD£¡ \n");
+	printf("This is a simple program.");
+	printf("My name is lv %d\n",num);
+	
+	return 0;
+} 
